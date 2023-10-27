3468873bdc08f0372c4d5c508f452796a2092275
Ben_Gittins_personal <bengittins@live.com>
2023-10-26 17:20:43 -0700

    Add basic react app

---
+++ src/properboozer.com/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App d-flex justify-content-center">
+      <h1 className='display-3 align-items'>properboozer.com</h1>
+    </div>
+  );
+}
+
+export default App;
