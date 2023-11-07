8d1b4f33e858b8dae96a9f29f5043e9561527bcc
Ben_Gittins_personal <bengittins@live.com>
2023-11-06 16:48:54 -0800

    Use context to store pubs data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+
+import { React, useState, useEffect } from 'react';
 
 import {
   Route, BrowserRouter as Router, Routes, Redirect,
 } from 'react-router-dom';
 
+import context from "./context/context"
 import NavBar from "./components/NavBar";
 import Home from './components/Home';
 import PubList from './components/PubList';
@@ -11,17 +13,36 @@ import Pub from "./components/Pub";
 import './App.css';
 
 function App() {
+  // api call on load
+  const [pubs, setPubs] = useState([])
+
+  const getpubs = async () => {
+    const res = await fetch('http://localhost:8000/pubs')
+    const resPubs = await res.json()
+    console.log(resPubs[0])
+    setPubs(resPubs)
+  }
+
+  useEffect(() => {
+    console.log("useEffect ran")
+    getpubs()
+  }, [])
+
+  const { Provider } = context
+
   return (
-    <Router>
-      <div className="App d-flex flex-column h-100 lead">
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/pubs" element={<PubList />} />
-          <Route path="/pubs/:url" element={<Pub />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider value={pubs}>
+      <Router>
+        <div className="App d-flex flex-column h-100 lead">
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/pubs" element={<PubList />} />
+            <Route path="/pubs/:url" element={<Pub />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- client/src/components/PubList.js
+++ client/src/components/PubList.js
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
 import PubPreview from './PubPreview';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import context from "../context/context"
 
 
 const PubList = () => {
+    // // const [pubs, setPubs] = useState([])
     // const [pubs, setPubs] = useState([])
-    const [pubs, setPubs] = useState([])
 
-    const getpubs = async () => {
-        const res = await fetch('http://localhost:8000/pubs')
-        const resPubs = await res.json()
-        console.log(resPubs[0])
-        setPubs(resPubs)
-    }
+    // const getpubs = async () => {
+    //     const res = await fetch('http://localhost:8000/pubs')
+    //     const resPubs = await res.json()
+    //     console.log(resPubs[0])
+    //     setPubs(resPubs)
+    // }
 
-    useEffect(() => {
-        console.log("useEffect ran")
-        getpubs()
-    }, [])
+    // useEffect(() => {
+    //     console.log("useEffect ran")
+    //     getpubs()
+    // }, [])
+    const pubs = useContext(context)
 
     return (
         <div className='PubsList'>
