52d60b6e2601d23b5a9e8502d33cd8b8625a7aff
Ben_Gittins_personal <bengittins@live.com>
2023-11-09 17:03:05 -0800

    Style home page.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,18 +1,18 @@
 const Card = (props) => {
     return (
-        // <div class="container">
+        // <div className="container">
 
-        //     <div class="row">
-        <div class="col-lg-4 mb-3 d-flex align-items-stretch">
-            <div class="card">
-                <img src={props.img_link} class="card-img-top" alt="Card Image" />
-                <div class="card-body d-flex flex-column justify-content-center">
-                    <h5 class="card-title">{props.title}</h5>
-                    <p class="card-text ">{props.text}</p>
-                    {/* <a href="#" class="btn btn-primary mt-auto align-self-start">Book now</a> */}
+        //     <div className="row">
+        // <div className="col-lg-4 mb-3 d-flex align-items-stretch">
+            <div className="card">
+                <img src={props.img_link} className="card-img-top" alt="Card Image" />
+                <div className="card-body d-flex flex-column justify-content-center">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text ">{props.text}</p>
+                    {/* <a href="#" className="btn btn-primary mt-auto align-self-start">Book now</a> */}
                 </div>
             </div>
-        </div>
+        // </div>
 
 
         // </div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 
 import Card from './Card'
+import PubBanner from '../images/princess_louise_crop.jpeg'
 
 const Home = () => {
     return (
         <>
-            <div className='Home col-8 align-self-center mt-4'>
-                <h1 className='display-3 '>properboozer.com</h1>
-                <p className=''>
+            <div className='Home col-md-8 col-12 align-self-center mt-4'>
+                <div className="home-img col-md-10 mx-auto">
+                    <img className="img-fluid" src={PubBanner} alt="Pub" />
+                </div>
+                <h1 className='display-5 m-3'>properboozer.com</h1>
+                <p className='col-10 mx-auto col-md-12' id='homePageText'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                     ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                     laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
@@ -16,12 +20,16 @@ const Home = () => {
                 </p>
             </div>
             <div className='CardList col-10 align-self-center'>
-                <div class="row pt-5">
-                    <h3 class="mb-4 pt-2">Equal height Bootstrap 5 cards example</h3>
-                    <Link to="/pubs" >
+                <div class="row pt-3">
+                    <Link className="col-lg-4 mb-3 d-flex align-items-stretch" to="/pubs" >
                         <Card img_link={"https://i.postimg.cc/28PqLLQC/dotonburi-canal-osaka-japan-700.jpg"} title={"title"} text={"textetststststst"} />
                     </Link>
-
+                    <Link className="col-lg-4 mb-3 d-flex align-items-stretch" to="/pubs" >
+                        <Card img_link={"https://i.postimg.cc/28PqLLQC/dotonburi-canal-osaka-japan-700.jpg"} title={"title"} text={"textetststststst"} />
+                    </Link>
+                    <Link className="col-lg-4 mb-3 d-flex align-items-stretch" to="/pubs" >
+                        <Card img_link={"https://i.postimg.cc/28PqLLQC/dotonburi-canal-osaka-japan-700.jpg"} title={"title"} text={"textetststststst"} />
+                    </Link>
                     {/* <Card img_link={"https://i.postimg.cc/28PqLLQC/dotonburi-canal-osaka-japan-700.jpg"} title={"title"} text={"textetstststststtextetstststststtextetstststststtextetstststststtextetstststststtextetstststststtextetstststststtextetststststst"} />
                     <Card img_link={"https://i.postimg.cc/28PqLLQC/dotonburi-canal-osaka-japan-700.jpg"} title={"title"} text={"textetstststststtextetstststststtextetstststststtextetstststststtextetstststststtextetstststststtextetstststststtextetststststst"} /> */}
                 </div>
