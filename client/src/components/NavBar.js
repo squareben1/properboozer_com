2c2d272f1241eadbedd0fa5fba0021295a015634
Ben_Gittins_personal <bengittins@live.com>
2023-11-09 17:26:35 -0800

    Style NavBar.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,29 +1,26 @@
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">
-                    properboozer.com
-                </Link>
-                <div className="navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        {/* <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">
-                                Home
-                            </Link>
-                        </li> */}
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/pubs">
-                                Pubs
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <Link className="nav-link active fw-bold" aria-current="page" to="/">properboozer.com</Link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul class="navbar-nav mb-1 mb-lg-0 ">
+            <li class="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/pubs">Pubs</Link>
+            </li>
+            <li class="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Museums</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
