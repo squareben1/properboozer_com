4afbf443cdf325ae36153e6769891bd6682a711f
Ben_Gittins_personal <bengittins@live.com>
2023-11-27 17:12:09 -0800

    gitignore node moudle and package json. Link API to DB.

--- api/app.js
+++ api/app.js
@@ -1,28 +1,17 @@
-let pubData = require('./data/pubData.json')
-
+// let pubData = require('./data/pubData.json')
 const express = require('express')
 const app = express();
+const PORT = process.env.PORT || 8000;
+let routes = require("./routes.js")
 
 const cors = require("cors")
 
-app.use(cors())
+app.use(cors({ origin: "http://localhost:3000" }))
 
+app.use("/", routes);
 app.use(express.json());
 
-const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
-
-app.get('/', (req, res) => {
-    res.send({ "Status": "API up and running!" })
-})
-
-app.get('/pubs/', (req, res) => {
-    res.send(pubData)
-})
-
-app.get('/pubs/:id', (req, res) => {
-    res.send({ "test": `get pub ${req.params.id}` })
-})
